cae513ec2407fecc3d53c1b657e483f4d1d10d58
EC2 Default User <ec2-user@ip-172-31-1-119.eu-west-1.compute.internal>
2025-04-03 10:21:13 +0000

    commit

--- expenses/models.py
+++ expenses/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Expense(models.Model):
-    """ class for expense""
     CATEGORY_CHOICES = [
         ('Food', 'Food'),
         ('Travel', 'Travel'),
