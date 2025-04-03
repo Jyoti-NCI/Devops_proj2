76c7117f4fafde8b1751c3014a6cf6db9ba0392f
EC2 Default User <ec2-user@ip-172-31-1-119.eu-west-1.compute.internal>
2025-04-03 09:54:10 +0000

    chages

--- expenses/models.py
+++ expenses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Expense(models.Model):
+    """ class for expense""
     CATEGORY_CHOICES = [
         ('Food', 'Food'),
         ('Travel', 'Travel'),
